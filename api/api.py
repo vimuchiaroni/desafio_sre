2ef80200b66a3efa78e3f5cce80114cdbfdeaf00
vimuchiaroni <victormuchiaroni@hotmail.com>
2021-04-12 18:23:20 -0300

    Adding API initial code

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
 import app.collect_data as createDB
 from flask import Flask
 import logging
+import re
 from flask import jsonify
 from flask import request
 
@@ -35,13 +36,35 @@ def listaRacas():
         return jsonify(db.document)
 
 
-@app.route('/api/temperamento/<tipo_temperamento>')
-def temperamento(tipo_temperamento):
+@app.route('/api/temperamento', methods=['get'])
+def getTemperamento():
+    tipo_temperamento = request.args.get('temperamento')
+    gatos = {}
+    if tipo_temperamento:
+        try:
+            for gato in db.document:
+                if re.search(rf'\b{tipo_temperamento}\b', db.document[gato][0]['temperamento'], flags=re.IGNORECASE):
+                    gatos[gato] = db.document[gato]
+        except Exception as err:
+            return jsonify("Nenhum gato encontrado com o temperamento informado")
+
+    else:
+        temperamentos = []
+        for racas in db.document:
+            for temperamento in db.document[racas][0]['temperamento'].split(','):
+                if temperamento.strip() not in temperamentos:
+                    temperamentos.append(temperamento.strip())
+        return jsonify(f'Temperamentos Disponiveis: {temperamentos}')
+
+
+
+
+
+    return gatos
+@app.route('/api/origem', methods=['get'])
+def getOrigem():
     pass
 
-@app.route('/api/version')
-def getVersion():
-    pass
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', use_reloader=False)
